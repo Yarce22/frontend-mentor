3abaddbaaa88eabf2028d94a3659d093de986e32
Alejandro Mira <alejomira96@gmail.com>
2024-12-30 18:21:53 -0500

    inicializacion del proyecto Pod request access

---
+++ pod-request-access-landing-page/tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+    color: {
+      Green: '#54E6AF',
+      Blue950: '#121725',
+      Blue900: '#2C344B',
+      Blue600: '#5A668A',
+      Blue300: '#C2CBE5',
+      White: '#FFF',
+      Red: '#FB3E3E',
+    },
+    fontFamily: {
+      Chivo: '"Chivo", serif;',
+    }
+  },
+  plugins: [],
+}
+
